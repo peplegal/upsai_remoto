31d857b6ec72b0fc7ea339b38e88a82bd50067af
Pep Legal <37920480+peplegal@users.noreply.github.com>
2026-06-03 03:16:00 -0300

    Update __init__.py

--- custom_components/upsai_remoto/__init__.py
+++ custom_components/upsai_remoto/__init__.py
@@ -60,12 +60,12 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
 
     # Tell Home Assistant to forward this setup to our upcoming sensor/switch files
-    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor", "switch"])
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    unload_ok = await hass.config_entries.async_unload_platforms(entry, ["sensor"])
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, ["sensor", "switch"])
     if unload_ok:
         hass.data[DOMAIN].pop(entry.entry_id)
     return True
