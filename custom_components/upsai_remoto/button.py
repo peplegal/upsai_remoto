568302ea26157cc3093b4c4bca3013d56978fadc
Pep Legal <37920480+peplegal@users.noreply.github.com>
2026-06-04 22:48:28 -0300

    Add files via upload

--- custom_components/upsai_remoto/button.py
+++ custom_components/upsai_remoto/button.py
@@ -32,7 +32,7 @@ class UpsaiMasterUnlockButton(ButtonEntity):
         self._engine = engine
         self._device_id = device_id
         
-        self._attr_name = "destravar"
+        self._attr_name = "Destravar"
         self._attr_unique_id = f"{device_id.lower()}_master_unlock"
         self._attr_icon = "mdi:lock-open-check"
         self._attr_device_info = DeviceInfo(
@@ -59,7 +59,7 @@ class UpsaiOutletRebootButton(ButtonEntity):
         self._device_id = device_id
         self._outlet_id = outlet_id
         
-        self._attr_name = f"_Reiniciar {outlet_id}"
+        self._attr_name = f"Reiniciar {outlet_id}"
         self._attr_unique_id = f"{device_id.lower()}_reboot0_{outlet_id}"
         self._attr_icon = "mdi:restart"
         self._attr_device_info = DeviceInfo(
